3b6b412f890eada5890a68bf06308238cbb246dd
xxrose <oleg419583@yandex.ru>
2021-09-05 14:13:15 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -5,20 +5,35 @@ import {CardGroup, OddsCalculator} from 'poker-odds-calculator';
 class App extends React.Component{
   constructor(props) {
     super(props);
-    this.state = [];
+    this.state = {player1 : '', player2 : '', board : '', player1ev : '', player2ev : ''};
+    this.onPlayer1 = this.onPlayer1.bind(this);
+    this.onPlayer2 = this.onPlayer2.bind(this);
+    this.onBoardChange = this.onBoardChange.bind(this);
+    this.evClick = this.evClick.bind(this);
   }
-  componentDidMount() {
-    const player1Cards = CardGroup.fromString('JhJs');
-    const player2Cards = CardGroup.fromString('JdQd');
-    const board = CardGroup.fromString('7d9dTsTdJc');
-
-    const result = OddsCalculator.calculate([player1Cards, player2Cards], board);
-
-    console.log(`Player #1 - ${player1Cards} - ${result.equities[0].getEquity()}%`);
-    console.log(`Player #2 - ${player2Cards} - ${result.equities[1].getEquity()}%`);
-
+  onPlayer1(event) {
+      this.setState({player1: event.target.value });
   }
+  onPlayer2(event) {
+      this.setState({player2: event.target.value });
+  }
+  onBoardChange(event) {
+      this.setState({board: event.target.value });
+  }
+  evClick() {
+      const player1 = CardGroup.fromString(this.state.player1);
+      const player2 = CardGroup.fromString(this.state.player2);
+      const board = CardGroup.fromString(this.state.board);
 
+      const result = OddsCalculator.calculate([player1,player2], board);
+
+      console.log(`Player #1 - ${player1} - ${result.equities[0].getEquity()}%`);
+      console.log(`Player #2 - ${player2} - ${result.equities[1].getEquity()}%`);
+      this.setState({player1ev: result.equities[0].getEquity() + '%', player2ev : result.equities[1].getEquity() + '%'})
+      console.log(this.state.player1)
+      console.log(this.state.player2)
+      console.log(this.state.board)
+  }
   render() {
     return(
        <div className='main__container'>
@@ -26,17 +41,20 @@ class App extends React.Component{
               <div className='poker__container'>
                   <div className='data__container'>
                       <label className='data__label'>Player 1</label>
-                      <input placeholder='Карты первого игрока'/>
+                      <input type='text' onChange={this.onPlayer1} placeholder='Карты первого игрока'/>
+                      <label className='data__label ev'>{this.state.player1ev}</label>
                   </div>
                   <div className='data__container' >
                       <label className='data__label'>Player 2</label>
-                      <input placeholder='Карты второго игрока'/>
+                      <input type='text' onChange={this.onPlayer2} placeholder='Карты второго игрока'/>
+                      <label className='data__label ev'>{this.state.player2ev}</label>
                   </div>
                   <div className='data__container'>
                       <label className='data__label'>Board</label>
-                      <input placeholder='Доска'/>
+                      <input type='text' onChange={this.onBoardChange} placeholder='Доска'/>
+                      <label className='data__label'></label>
                   </div>
-                  <button className='data__ev__button'>EV calc</button>
+                  <button onClick={this.evClick} className='data__ev__button'>EV calc</button>
               </div>
        </div>
     )
